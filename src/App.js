c0988aca0992c5d9ec306beed732c9afc361987b
Sahitya77 <sahityanijhawan@gmail.com>
2022-06-06 13:51:20 +0530

    new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import Todos from './components/Todos';
 import './App.css';
 import React from 'react';
 function App() {
   return (
-    <div className="App">
-      <Todos/>
+    <div className="main_dev">
+      <div className='center_div'>
+        <br/>
+        <h1>Todo List</h1>
+        <br/>
+        <input type='text' placeholder="Add a items" />
+        <button>+</button>
+        <ol>
+          <li>
+            buy apple
+          </li>
+        </ol>
+      </div>
     </div>
   );
 }
